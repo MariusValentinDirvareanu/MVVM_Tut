a9b1f4d1112a20fbf1752364cbe10b47a8d23b0f
MariusValentinDirvareanu <dirvareanumarius@gmail.com>
2019-04-11 20:09:33 +0300

    1

---
+++ 1/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using _1.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1
+{
+    public class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
